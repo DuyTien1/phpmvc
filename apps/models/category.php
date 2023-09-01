20bcd49b33582c22062b384f44643a604d3845fd
Tong Nguyen <thanhtong531@gmail.com>
2023-09-01 08:59:12 +0700

    controller content

--- apps/models/category.php
+++ apps/models/category.php
@@ -1,45 +1,53 @@
 <?php
-class Category {
+class Category
+{
     public $id;
     public $category_name;
 
-    function __construct($id, $category_name){
-    $this->id = $id;
-    $this->category_name = $category_name;
+    function __construct($id, $category_name)
+    {
+        $this->id = $id;
+        $this->category_name = $category_name;
     }
 
-    public static function all() {
-    $list = [];
-    $db = DB::getInstance();
-    $req = $db->query('SELECT * FROM categories');
-    foreach ($req->fetchAll() as $item) {
-        $list[] = new User($item['id'], $item['category_name']);
-        }
-    return $list;
-    }
-
-    public static function create($category_name, $user_id) {
+    public static function all()
+    {
+        $list = [];
         $db = DB::getInstance();
-        $req = $db->prepare('INSERT INTO categories (category_name) VALUES (:category_name)');
-        $req->execute(array('category_name' => $category_name));
-        return null;
-    } 
-
-    public static function find($id) {
-    $db = DB::getInstance();
-    $req = $db->prepare('SELECT * FROM categories WHERE id = :id');
-    $req->execute(array('id' => $id));
-    $item = $req->fetch();
-    if (isset($item['id'])) {
-        return new Category($item['id'], $item['category_name']);
+        $req = $db->query('SELECT * FROM categories');
+        foreach ($req->fetchAll() as $item) {
+            $list[] = new Category($item['id'], $item['category_name']);
         }
-    return null;
-    } 
+        return $list;
+    }
 
-    public static function destroy($id) {
+    public static function create($category_name)
+    {
+        $db = DB::getInstance();
+        $req = $db->prepare(
+            'INSERT INTO categories (category_name) VALUES (:category_name)'
+        );
+        $req->execute(['category_name' => $category_name]);
+        return null;
+    }
+
+    public static function find($id)
+    {
+        $db = DB::getInstance();
+        $req = $db->prepare('SELECT * FROM categories WHERE id = :id');
+        $req->execute(['id' => $id]);
+        $item = $req->fetch();
+        if (isset($item['id'])) {
+            return new Category($item['id'], $item['category_name']);
+        }
+        return null;
+    }
+
+    public static function destroy($id)
+    {
         $db = DB::getInstance();
         $req = $db->prepare('DELETE FROM categories WHERE id = :id');
-        $req->execute(array('id' => $id));
+        $req->execute(['id' => $id]);
         return null;
     }
 
@@ -53,5 +61,5 @@ class Category {
     //     } else {
     //         return null;
     //     }
-    // }  
+    // }
 }
--- apps/models/token.php
+++ apps/models/token.php
@@ -5,68 +5,79 @@ class Token
     public $token;
     public $user_id;
 
-    function __construct($id, $token, $user_id){
-    $this->id = $id;
-    $this->token = $token;
-    $this->user_id = $user_id;
+    function __construct($id, $token, $user_id)
+    {
+        $this->id = $id;
+        $this->token = $token;
+        $this->user_id = $user_id;
     }
 
-    public static function all() {
-    $list = [];
-    $db = DB::getInstance();
-    $req = $db->query('SELECT * FROM tokens');
-    foreach ($req->fetchAll() as $item) {
-        $list[] = new User($item['id'], $item['token'],$item['user_id']);
-        }
-    return $list;
-    }
-
-    public static function create($token, $user_id) {
+    public static function all()
+    {
+        $list = [];
         $db = DB::getInstance();
-        $req = $db->prepare('INSERT INTO tokens (token, user_id) VALUES (:token, :user_id)');
-        $req->execute(array('token' => $token, 'user_id' => $user_id));
+        $req = $db->query('SELECT * FROM tokens');
+        foreach ($req->fetchAll() as $item) {
+            $list[] = new Token($item['id'], $item['token'], $item['user_id']);
+        }
+        return $list;
+    }
+
+    public static function create($token, $user_id)
+    {
+        $db = DB::getInstance();
+        $req = $db->prepare(
+            'INSERT INTO tokens (token, user_id) VALUES (:token, :user_id)'
+        );
+        $req->execute(['token' => $token, 'user_id' => $user_id]);
         // $item = $req->fetch();
         return null;
-    } 
-
-    public static function find($id) {
-    $db = DB::getInstance();
-    $req = $db->prepare('SELECT * FROM tokens WHERE id = :id');
-    $req->execute(array('id' => $id));
-    $item = $req->fetch();
-    if (isset($item['id'])) {
-        return new Token($item['id'], $item['token'], $item['user_id']);
-        }
-    return null;
-    } 
-
-    public static function destroy($id) {
-        $db = DB::getInstance();
-        $req = $db->prepare('DELETE FROM tokens WHERE id = :id');
-        $req->execute(array('id' => $id));
-        return null;
     }
-    
-    public static function findToken($token) {
+
+    public static function find($id)
+    {
         $db = DB::getInstance();
-        $req = $db->prepare('SELECT * FROM tokens WHERE token = :token');
-        $req->execute(array('token' => $token));
+        $req = $db->prepare('SELECT * FROM tokens WHERE id = :id');
+        $req->execute(['id' => $id]);
         $item = $req->fetch();
         if (isset($item['id'])) {
             return new Token($item['id'], $item['token'], $item['user_id']);
-            }
+        }
         return null;
     }
 
-    public static function tokenVerify($token) {
+    public static function destroy($id)
+    {
         $db = DB::getInstance();
-        $req = $db->prepare('SELECT tokens.*, users.* FROM tokens JOIN Users ON tokens.user_id = users.id WHERE tokens.id = :token');
-        $req->execute(array('token' => $token));
+        $req = $db->prepare('DELETE FROM tokens WHERE id = :id');
+        $req->execute(['id' => $id]);
+        return null;
+    }
+
+    public static function findToken($token)
+    {
+        $db = DB::getInstance();
+        $req = $db->prepare('SELECT * FROM tokens WHERE token = :token');
+        $req->execute(['token' => $token]);
+        $item = $req->fetch();
+        if (isset($item['id'])) {
+            return new Token($item['id'], $item['token'], $item['user_id']);
+        }
+        return null;
+    }
+
+    public static function tokenVerify($token)
+    {
+        $db = DB::getInstance();
+        $req = $db->prepare(
+            'SELECT tokens.*, users.* FROM tokens JOIN Users ON tokens.user_id = users.id WHERE tokens.id = :token'
+        );
+        $req->execute(['token' => $token]);
         $item = $req->fetch();
         if (isset($item['user_id'])) {
             return $item;
         } else {
             return null;
         }
-    }  
+    }
 }
